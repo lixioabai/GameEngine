8744e0905141a90537ec58051d282a7cbfa85767
Liboyan <liboyan@zte9.com>
2018-11-09 15:44:32 +0800

    Update Assets/GameEngine/UIFramwork/Tools/UITools.cs
    UI 高亮按钮的集成化工具

---
+++ Assets/GameEngine/UIFramwork/Tools/UITools.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 关于UI 的工具
+/// </summary>
+public class UITools : MonoBehaviour
+{
+
+
+
+    /// <summary>
+    /// 改变高亮和自身图片
+    /// </summary>
+    /// <param name="picturePath"></param>
+    /// <param name="picturelinePath"></param>
+    public static void ChangeButtonPicture(Button btnName,string picturePath, string picturelinePath)
+    {
+        btnName.GetComponent<Image>().sprite = Resources.Load<Sprite>(picturePath);
+        SpriteState spState = new SpriteState();
+        btnName.transition = Selectable.Transition.SpriteSwap;
+        spState.highlightedSprite = Resources.Load<Sprite>(picturelinePath);
+        btnName.spriteState = spState;
+    }
+}
